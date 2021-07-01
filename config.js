1b3bbca33624eda0e9aa6f30a46b87f297e2f2c6
baotuyet0302 <86763979+baotuyet0302@users.noreply.github.com>
2021-07-01 13:14:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bé à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,75 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Dạ bé thích anh <33',
+    btnNo: 'Không, Anh à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Em yêu Anh',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Từ bây giờ trở về sau anh chỉ yêu mỗi mình em thôi . Em đồng ý làm ny anh nhé <31
+
+const CONFIG = {
+
+2
+
+    introTitle: 'Bé à!',
+
+3
+
+    introDesc: `Trái đất vốn lạ thường
+
+4
+
+    Mà sao em cứ đi nhầm đường
+
+5
+
+    Lạc vào tim anh lẻ loi
+
+6
+
+    Đằng sau chữ yêu đây là thương`,
+
+7
+
+    btnIntro: 'hihi',
+
+8
+
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+
+9
+
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+
+10
+
+    btnYes: 'Dạ bé thích anh <33',
+
+11
+
+    btnNo: 'Không, Anh à :3',
+
+12
+
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+
+13
+
+    btnReply: 'Gửi cho anh <3',
+
+14
+
+    reply: 'Em yêu Anh',
+
+15
+
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+
+16
+
+    messDesc: 'Từ bây giờ trở về sau anh chỉ yêu mỗi mình em thôi . Em đồng ý làm ny anh nhé <3.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://baokun0310 //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
